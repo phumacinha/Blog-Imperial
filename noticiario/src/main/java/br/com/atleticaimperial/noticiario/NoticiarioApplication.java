6a438cfd88f04ce280a61e7f6d6c1c0fd40d3861
Pedro Antônio de Souza <pedro.souza2@estudante.ufla.br>
2020-12-12 23:47:39 -0300

    spring initializer

---
+++ noticiario/src/main/java/br/com/atleticaimperial/noticiario/NoticiarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.atleticaimperial.noticiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiarioApplication.class, args);
+	}
+
+}
