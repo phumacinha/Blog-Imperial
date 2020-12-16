ba6904e2f4350f9c13d043bb42ad6e4a4ee1f834
Pedro Antônio de Souza <pedro.souza2@estudante.ufla.br>
2020-12-16 11:39:37 -0300

    adicionada fonte montserrat com peso 900

--- noticiario/src/App.vue
+++ noticiario/src/App.vue
@@ -27,7 +27,7 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&family=Teko:wght@400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;0,900;1,400;1,700&family=Teko:wght@400;500&display=swap');
 @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
 
 body {
